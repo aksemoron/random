db9330e94d48659dc9222234a8b2529da5c99545
andrey <89146748462@ya.ru>
2021-03-08 11:11:10 +0300

    inital

--- src/main/java/com/serverForSlot/random/Controller.java
+++ src/main/java/com/serverForSlot/random/Controller.java
@@ -9,6 +9,6 @@ public class Controller {
 
     @GetMapping("/")
     public ResponseEntity hello(){
-        return ResponseEntity.ok("Пошел нахуй");
+        return ResponseEntity.ok("Пss");
     }
 }
